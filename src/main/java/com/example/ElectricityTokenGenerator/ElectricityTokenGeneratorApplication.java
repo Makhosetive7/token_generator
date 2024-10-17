0ba229d91f1d17caaecff9f3df8677a921bf0e64
Makhosetive7 <sibandamakhosetive7@gmail.com>
2024-10-17 15:00:36 +0200

    setting my first Java-spring project

---
+++ src/main/java/com/example/ElectricityTokenGenerator/ElectricityTokenGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElectricityTokenGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityTokenGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityTokenGeneratorApplication.class, args);
+	}
+
+}
